aaa0bfe1fdd680ee203f4f221b1dbc95befd6aee
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-02-04 18:52:58 +0800

    add

---
+++ Tabulation.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+using System.Web.UI; 
+namespace Tabulation
+{
+    public class Tabulation
+    {
+        public static DataTable NewTable(int NumRows, int NumCols){
+            //NewTable的作用是创建一张空白表格
+            //NumRows为添加的行数，NumCols为添加的列数
+            DataTable dt = new DataTable();
+            int i;
+            for (i = 0; i < NumCols; i++)
+            {
+                dt.Columns.Add("");
+            }
+            //添加列
+            //默认会添加Colum1，Colum2，……
+            for (i = 0; i < NumRows; i++)
+            {
+                dt.Rows.Add("");
+            }
+            //添加行
+            //要多少列和行就add多少就好
+            return dt;
+        }
+        public static DataTable ImportExcel(string filePath,string SheetName)
+        {
+            //该函数的目的是为了导入Excel表格，并返回DataTable
+            //根据路径打开一个Excel文件并将数据填充到DataSet中
+            //filePath为文件路径，SheetName为表格名称
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source = " + filePath + ";Extended Properties ='Excel 8.0;HDR=NO;IMEX=1'";//导入时包含Excel中的第一行数据，并且将数字和字符混合的单元格视为文本进行导入
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            //strExcel = "select  * from   [sheet1$]";
+            strExcel = "select  * from   [" + SheetName + "$]";
+            //在这里选择Excel表格的名称
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            //根据DataGridView的列构造一个新的DataTable
+            DataTable tb = new DataTable();
+            for (int i = 0; i < ds.Tables[0].Columns.Count; i++)
+            {
+                DataColumn dc = new DataColumn();
+                //dc.DataType = dgvc.ValueType;//若需要限制导入时的数据类型则取消注释，前提是DataGridView必须先绑定一个数据源那怕是空的DataTable
+                tb.Columns.Add(dc);
+            }
+
+            //根据Excel的行逐一对上面构造的DataTable的列进行赋值
+            foreach (DataRow excelRow in ds.Tables[0].Rows)
+            {
+                int i = 0;
+                DataRow dr = tb.NewRow();
+                foreach (DataColumn dc in tb.Columns)
+                {
+                    dr[dc] = excelRow[i];
+                    i++;
+                }
+                tb.Rows.Add(dr);
+            }
+            //在DataGridView中显示导入的数据
+            //dgv.DataSource = tb;
+            return tb;
+            //返回DataTable
+        }
+        public static DataTable ImportCSV()
+        {
+            string fileToLoad = " ";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "csv文件(*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileToLoad = openFileDialog1.FileName;
+                LoadFromCSVFile(fileToLoad);
+                return LoadFromCSVFile(fileToLoad); ;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static DataTable LoadFromCSVFile(string pCsvPath)
+        {
+
+            DataTable dt = new DataTable();
+            try
+            {
+                String line;
+                String[] split = null;
+                DataRow row = null;
+                StreamReader sr = new StreamReader(pCsvPath, System.Text.Encoding.Default);
+                //创建与数据源对应的数据列 
+                line = sr.ReadLine();
+                split = line.Split(',');
+                foreach (String colname in split)
+                {
+                    dt.Columns.Add(colname, System.Type.GetType("System.String"));
+                }
+                //将数据填入数据表 
+                int j = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    j = 0;
+                    row = dt.NewRow();
+                    split = line.Split(',');
+                    foreach (String colname in split)
+                    {
+                        row[j] = colname;
+                        j++;
+                    }
+                    dt.Rows.Add(row);
+                }
+                sr.Close();
+                //显示数据 
+                //this.dataGridView1.DataSource = table.DefaultView;
+                return dt;
+            }
+            catch (Exception vErr)
+            {
+                MessageBox.Show(vErr.Message);
+                return null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+        public static void ExportCSV(DataTable dt)
+        {
+            string path = "";
+
+            //File info initialization
+            SaveFileDialog ofd = new SaveFileDialog();
+            ofd.Title = "csv文件";
+            ofd.FileName = "";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);//为了获取特定的系统文件夹，可以使用System.Environment类的静态方法GetFolderPath()。该方法接受一个Environment.SpecialFolder枚举，其中可以定义要返回路径的哪个系统目录
+            ofd.Filter = "csv文件(*.csv)|*.csv";
+            ofd.ValidateNames = true;     //文件有效性验证ValidateNames，验证用户输入是否是一个有效的Windows文件名
+            ofd.CheckFileExists = true;  //验证路径有效性
+            ofd.CheckPathExists = true; //验证文件有效性
+
+
+            string strName = string.Empty;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                strName = ofd.FileName;
+            }
+
+            if (strName == "")
+            {
+                MessageBox.Show("没有选择Excel文件！无法进行数据导入");
+                return;
+            }
+            path = strName;
+
+
+
+            System.IO.FileStream fs = new FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs, new System.Text.UnicodeEncoding());
+            //Tabel header
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                sw.Write(dt.Columns[i].ColumnName);
+                sw.Write("\t");
+            }
+            sw.WriteLine("");
+            //Table body
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    sw.Write(DelQuota(dt.Rows[i][j].ToString()));
+                    sw.Write("\t");
+                }
+                sw.WriteLine("");
+            }
+            sw.Flush();
+            sw.Close();
+
+            DownLoadFile(path);
+        }
+
+        private static bool DownLoadFile(string _FileName)
+        {
+            //导出CSV文件需要使用该函数
+            try
+            {
+                System.IO.FileStream fs = System.IO.File.OpenRead(_FileName);
+                byte[] FileData = new byte[fs.Length];
+                fs.Read(FileData, 0, (int)fs.Length);
+                System.Web.HttpContext.Current.Response.Clear();
+                System.Web.HttpContext.Current.Response.AddHeader("Content-Type", "application/notepad");
+                string FileName = System.Web.HttpUtility.UrlEncode(System.Text.Encoding.UTF8.GetBytes(_FileName));
+                System.Web.HttpContext.Current.Response.AddHeader("Content-Disposition", "inline;filename=" + System.Convert.ToChar(34) + FileName + System.Convert.ToChar(34));
+                System.Web.HttpContext.Current.Response.AddHeader("Content-Length", fs.Length.ToString());
+                System.Web.HttpContext.Current.Response.BinaryWrite(FileData);
+                fs.Close();
+                System.IO.File.Delete(_FileName);
+                System.Web.HttpContext.Current.Response.Flush();
+                System.Web.HttpContext.Current.Response.End();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+                return false;
+            }
+        }
+        public static string DelQuota(string str)
+        {
+            //Delete special symbol
+            //导出CSV文件需要使用该函数
+            string result = str;
+            string[] strQuota = { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "`", ";", "'", ",", ".", "/", ":", "/,", "<", ">", "?" };
+            for (int i = 0; i < strQuota.Length; i++)
+            {
+                if (result.IndexOf(strQuota[i]) > -1)
+                    result = result.Replace(strQuota[i], "");
+            }
+            return result;
+        }
+
+        private void ExportExcel(DataGridView dataGridView1)
+        {
+            //用于导出Excel文件
+            //参数为dataGridView
+            #region
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Execl files (*.xls)|*.xls";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.CreatePrompt = true;
+            saveFileDialog.Title = "Export Excel File";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName == "")
+                return;
+            Stream myStream;
+            myStream = saveFileDialog.OpenFile();
+            StreamWriter sw = new StreamWriter(myStream, System.Text.Encoding.GetEncoding(-0));
+
+            string str = "";
+            try
+            {
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    if (i > 0)
+                    {
+                        str += "\t";
+                    }
+                    str += dataGridView1.Columns[i].HeaderText;
+                }
+                sw.WriteLine(str);
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    string tempStr = "";
+                    for (int k = 0; k < dataGridView1.Columns.Count; k++)
+                    {
+                        if (k > 0)
+                        {
+                            tempStr += "\t";
+                        }
+                        tempStr += dataGridView1.Rows[j].Cells[k].Value.ToString();
+                    }
+                    sw.WriteLine(tempStr);
+                }
+                sw.Close();
+                myStream.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sw.Close();
+                myStream.Close();
+            }
+            #endregion
+        }
+        
+    }
+}
